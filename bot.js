c5a609607c9968efff07f20129b058657505bb5f
RyadForGamessss <53405623+RyadForGamessss@users.noreply.github.com>
2019-08-06 16:31:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,40 +11,40 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "488746365933846529") return;
+  if (message.author.id !== "376811690861527041") return;
 
   
-  if (message.content.startsWith(prefix + 'NotAnesSw')) {
+  if (message.content.startsWith(prefix + 'setwatch')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
 } 
 
  
-  if (message.content.startsWith(prefix + 'NotAnesSl')) {
+  if (message.content.startsWith(prefix + 'setlis')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
 } 
 
 
-if (message.content.startsWith(prefix + 'NotAnesSn')) {
+if (message.content.startsWith(prefix + 'RafaaeelChangeName')) {
   client.user.setUsername(argresult).then
-      message.channel.sendMessage(`:white_check_mark: ${argresult}`)
-  return message.reply("You Can change the username 2 times per hour");
+      message.channel.sendMessage(`:white_check_mark: ${argresult}**`)
+  return message.reply(":white_check_mark:");
 } 
 
-if (message.content.startsWith(prefix + 'NotAnesSa')) {
+if (message.content.startsWith(prefix + 'Rafaaeelsetavatar')) {
   client.user.setAvatar(argresult);
-   message.channel.sendMessage(`:white_check_mark: ${argresult}`;
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
-if (message.content.startsWith(prefix + 'NotAnesSt')) {
+if (message.content.startsWith(prefix + 'Rafaaeelstream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
 } 
-if (message.content.startsWith(prefix + 'NotAnesPl')) {
+if (message.content.startsWith(prefix + 'Rafaaeelsetplay')) {
   client.user.setGame(argresult);
      console.log('test' + argresult);
     message.channel.sendMessage(`:white_check_mark: ${argresult}`)
